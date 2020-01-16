6e2209dc3cb62ade49396fe0338e4636dc5d98d5
Yordan <yordantsvetomirov96@gmail.com>
2020-01-16 20:38:49 +0200

    Whole project

--- administration/user-profile.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once($_SERVER['DOCUMENT_ROOT'].'/administration/header.php');
-require_once($_SERVER['DOCUMENT_ROOT'].'/inc/glbFunc.php');
-
-
-require_once($_SERVER['DOCUMENT_ROOT'].'/administration/footer.php');
-?>
